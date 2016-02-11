95d833964677ada9f4ebc4b440e6432e8bffc564
Michael Merkushev <m.m.v2014@yandex.ua>
2016-02-11 14:49:57 +0200

    My homework

--- 20.php
+++ 20.php
@@ -1,9 +1,4 @@
 <?php
 $a=20;
 var_dump((bool)$a) ;
-/**
- * Created by PhpStorm.
- * User: New
- * Date: 09.02.2016
- * Time: 11:06
- */
+// true так как значение не равно null
---
+++ 27.php
@@ -0,0 +1,2 @@
+<?php
+ReflectionClass::getFileName;
